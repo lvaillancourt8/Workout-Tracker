a1a8c1fae9788d7abc07d095662c784d8501e906
lvaillan <classvaillan@gmail.com>
2021-10-29 15:35:17 -0500

    Get all workouts

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,4 +1,15 @@
+const db = require("../models");
 const router = require('express').Router();
 
+//Get all workouts
+router.get('/workouts', (req, res) => {
+    db.Workout.find({})
+    .then(Workoutdb => {
+      res.json(Workoutdb);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
 
 module.exports = router;
